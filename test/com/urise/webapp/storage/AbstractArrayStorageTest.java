dff1503c56312fc3ba9c6a07761ee6eb60af6a99
Dzianis Kazakevich <nicklazzdev@gmail.com>
2023-09-17 16:33:37 +0300

    Implemented static-block in AbstractArrayStorage

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -15,16 +15,20 @@ class AbstractArrayStorageTest {
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
-
     private static final String UUID_NOT_EXIST = "dummy";
-    private static final Resume RESUME_1 = new Resume(UUID_1);
-    private static final Resume RESUME_2 = new Resume(UUID_2);
-    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_1;
+    private static final Resume RESUME_2;
+    private static final Resume RESUME_3;
+
+    static {
+        RESUME_1 = new Resume(UUID_1);
+        RESUME_2 = new Resume(UUID_2);
+        RESUME_3 = new Resume(UUID_3);
+    }
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
     }
-
 
     @BeforeEach
     void setUp() {
