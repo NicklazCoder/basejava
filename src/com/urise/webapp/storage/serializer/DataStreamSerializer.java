8e10019178d471e3f432ba9dd56218c462a139f9
Dzianis Kazakevich <nicklazzdev@gmail.com>
2023-10-24 14:52:18 +0300

    Partially implemented DataStreamSerializer, it is necessary to complete the method doRead()

---
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -0,0 +1,104 @@
+package com.urise.webapp.storage.serializer;
+
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.*;
+
+import java.io.*;
+import java.time.LocalDate;
+import java.util.Collection;
+
+public class DataStreamSerializer implements StreamSerializer {
+    @Override
+    public void doWrite(Resume r, OutputStream os) throws IOException {
+        try (DataOutputStream dos = new DataOutputStream(os)) {
+            dos.writeUTF(r.getUuid());
+            dos.writeUTF(r.getFullName());
+            MapWriter(dos, r.getContacts().entrySet(), entry -> {
+                dos.writeUTF(entry.getKey().name());
+                dos.writeUTF(entry.getValue());
+            });
+            MapWriter(dos, r.getSections().entrySet(), entry -> {
+                SectionType type = entry.getKey();
+                Section section = entry.getValue();
+                dos.writeUTF(type.name());
+                switch (type) {
+                    case PERSONAL, OBJECTIVE -> dos.writeUTF(((TextSection) section).getContent());
+                    case ACHIEVEMENT, QUALIFICATIONS ->
+                            MapWriter(dos, ((ListSection) section).getElements(), dos::writeUTF);
+                    case EDUCATION, EXPERIENCE ->
+                            MapWriter(dos, ((OrganizationSection) section).getOrganizations(), entryOrg -> {
+                                dos.writeUTF(entryOrg.getName());
+                                dos.writeUTF(entryOrg.getHomePage());
+                                MapWriter(dos, entryOrg.getPeriods(), entryPeriods -> {
+                                    dos.writeUTF(entryPeriods.getTitle());
+                                    dos.writeUTF(entryPeriods.getDescription());
+                                    dos.writeUTF(entryPeriods.getStartDate().toString());
+                                    dos.writeUTF(entryPeriods.getEndDate().toString());
+                                });
+
+                            });
+                }
+            });
+        }
+    }
+
+    @Override
+    public Resume doRead(InputStream is) throws IOException {
+        try (DataInputStream dis = new DataInputStream(is)) {
+            String uuid = dis.readUTF();
+            String fullName = dis.readUTF();
+            Resume resume = new Resume(uuid, fullName);
+            ItemReader(dis, () -> resume.setContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            ItemReader(dis, () -> {
+                SectionType type = SectionType.valueOf(dis.readUTF());
+                //resume.setSections(type,);
+            });
+            return resume;
+        } catch (IOException e) {
+            throw new StorageException("Error read resume", e);
+        }
+    }
+
+
+    private interface ItemWriter<T> {
+        void write(T o) throws IOException;
+    }
+
+    private interface ItemProcess {
+        void process() throws IOException;
+    }
+
+    private interface ItemReader<T> {
+        T read() throws IOException;
+    }
+
+    private void ItemReader(DataInputStream dis, ItemProcess processor) throws IOException {
+        int size = dis.readInt();
+        for (int i = 0; i < size; i++) {
+            processor.process();
+        }
+    }
+
+    private <T> void MapWriter(DataOutputStream dos, Collection<T> collection, ItemWriter<T> writer) {
+        try {
+            dos.writeInt(collection.size());
+            for (T item : collection) {
+                writer.write(item);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //TODO
+    private Section SectionReader(DataInputStream dis, SectionType sectionType) throws IOException {
+        switch (sectionType) {
+            case PERSONAL, OBJECTIVE -> {
+                return new TextSection(dis.readUTF());
+            }
+            case ACHIEVEMENT, QUALIFICATIONS -> {
+                return null;
+            }
+            default -> throw new IllegalStateException();
+        }
+    }
+}
