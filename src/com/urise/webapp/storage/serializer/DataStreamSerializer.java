d0965a965c0e15e1734cb2ba917032216e5fdddc
Dzianis Kazakevich <nicklazzdev@gmail.com>
2023-10-31 20:45:50 +0300

    Refactoring DataStreamSerializer

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -12,49 +12,20 @@ public class DataStreamSerializer implements StreamSerializer {
     @Override
     public void doWrite(Resume r, OutputStream os) throws IOException {
         try (DataOutputStream dos = new DataOutputStream(os)) {
-            ContactsWriter(r, dos);
-            MapWriter(dos, r.getSections().entrySet(), entry -> {
+            writeContacts(r, dos);
+            writeCollection(dos, r.getSections().entrySet(), entry -> {
                 SectionType type = entry.getKey();
                 Section section = entry.getValue();
                 dos.writeUTF(type.name());
                 switch (type) {
-                    case PERSONAL, OBJECTIVE -> TextSectionWriter(dos, (TextSection) section);
-                    case ACHIEVEMENT, QUALIFICATIONS -> ListSectionWriter(dos, (ListSection) section);
-                    case EDUCATION, EXPERIENCE -> OrganizationSectionWriter(dos, (OrganizationSection) section);
+                    case PERSONAL, OBJECTIVE -> writeTextSection(dos, (TextSection) section);
+                    case ACHIEVEMENT, QUALIFICATIONS -> writeListSection(dos, (ListSection) section);
+                    case EDUCATION, EXPERIENCE -> writeOrganizationSection(dos, (OrganizationSection) section);
                 }
             });
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
-    }
-
-    private void OrganizationSectionWriter(DataOutputStream dos, OrganizationSection section) {
-        MapWriter(dos, section.getOrganizations(), entryOrg -> {
-            dos.writeUTF(entryOrg.getName());
-            dos.writeUTF(entryOrg.getHomePage());
-            MapWriter(dos, entryOrg.getPeriods(), entryPeriods -> {
-                dos.writeUTF(entryPeriods.getTitle());
-                dos.writeUTF(entryPeriods.getDescription());
-                dos.writeUTF(entryPeriods.getStartDate().toString());
-                dos.writeUTF(entryPeriods.getEndDate().toString());
-            });
-
-        });
-    }
-
-    private void ListSectionWriter(DataOutputStream dos, ListSection section) {
-        MapWriter(dos, section.getElements(), dos::writeUTF);
-    }
-
-    private void TextSectionWriter(DataOutputStream dos, TextSection section) throws IOException {
-        dos.writeUTF(section.getContent());
-    }
-
-    private void ContactsWriter(Resume r, DataOutputStream dos) throws IOException {
-        dos.writeUTF(r.getUuid());
-        dos.writeUTF(r.getFullName());
-        MapWriter(dos, r.getContacts().entrySet(), entry -> {
-            dos.writeUTF(entry.getKey().name());
-            dos.writeUTF(entry.getValue());
-        });
     }
 
     @Override
@@ -63,28 +34,58 @@ public class DataStreamSerializer implements StreamSerializer {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            ItemsReader(dis, () -> resume.setContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
-            ItemsReader(dis, () -> {
+            readItems(dis, () -> resume.setContact(ContactType.valueOf(dis.readUTF()), dis.readUTF()));
+            readItems(dis, () -> {
                 SectionType type = SectionType.valueOf(dis.readUTF());
-                resume.setSections(type, SectionReader(dis, type));
+                resume.setSections(type, readSection(dis, type));
             });
             return resume;
         }
     }
 
+    private void writeContacts(Resume r, DataOutputStream dos) throws IOException {
+        dos.writeUTF(r.getUuid());
+        dos.writeUTF(r.getFullName());
+        writeCollection(dos, r.getContacts().entrySet(), entry -> {
+            dos.writeUTF(entry.getKey().name());
+            dos.writeUTF(entry.getValue());
+        });
+    }
 
-    private Section SectionReader(DataInputStream dis, SectionType sectionType) throws IOException {
+    private void writeTextSection(DataOutputStream dos, TextSection section) throws IOException {
+        dos.writeUTF(section.getContent());
+    }
+
+    private void writeListSection(DataOutputStream dos, ListSection section) throws IOException {
+        writeCollection(dos, section.getElements(), dos::writeUTF);
+    }
+
+    private void writeOrganizationSection(DataOutputStream dos, OrganizationSection section) throws IOException {
+        writeCollection(dos, section.getOrganizations(), entryOrg -> {
+            dos.writeUTF(entryOrg.getName());
+            dos.writeUTF(entryOrg.getHomePage());
+            writeCollection(dos, entryOrg.getPeriods(), entryPeriods -> {
+                dos.writeUTF(entryPeriods.getTitle());
+                dos.writeUTF(entryPeriods.getDescription());
+                dos.writeUTF(entryPeriods.getStartDate().toString());
+                dos.writeUTF(entryPeriods.getEndDate().toString());
+            });
+        });
+    }
+
+
+    private Section readSection(DataInputStream dis, SectionType sectionType) throws IOException {
         switch (sectionType) {
             case PERSONAL, OBJECTIVE -> {
                 return new TextSection(dis.readUTF());
             }
             case ACHIEVEMENT, QUALIFICATIONS -> {
-                return new ListSection(ListReader(dis, dis::readUTF));
+                return new ListSection(readList(dis, dis::readUTF));
             }
             case EDUCATION, EXPERIENCE -> {
-                return new OrganizationSection(ListReader(dis, () ->
+                return new OrganizationSection(readList(dis, () ->
                         new Organization(dis.readUTF(), dis.readUTF(),
-                                ListReader(dis, () ->
+                                readList(dis, () ->
                                         new Period(dis.readUTF(), dis.readUTF(),
                                                 LocalDate.parse(dis.readUTF()), LocalDate.parse(dis.readUTF()))))));
             }
@@ -94,47 +95,36 @@ public class DataStreamSerializer implements StreamSerializer {
 
     private interface ItemWriter<T> {
         void write(T o) throws IOException;
-
     }
 
-    private interface ItemProcess {
+    private interface ItemProcessor {
         void process() throws IOException;
-
     }
 
     private interface ItemReader<T> {
         T read() throws IOException;
-
     }
 
-    private void ItemsReader(DataInputStream dis, ItemProcess processor) throws IOException {
+    private void readItems(DataInputStream dis, ItemProcessor processor) throws IOException {
         int size = dis.readInt();
         for (int i = 0; i < size; i++) {
             processor.process();
         }
     }
 
-    private <T> void MapWriter(DataOutputStream dos, Collection<T> collection, ItemWriter<T> writer) {
-        try {
-            dos.writeInt(collection.size());
-            for (T item : collection) {
-                writer.write(item);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private <T> void writeCollection(DataOutputStream dos, Collection<T> collection, ItemWriter<T> writer) throws IOException {
+        dos.writeInt(collection.size());
+        for (T item : collection) {
+            writer.write(item);
         }
     }
 
-    private <T> List<T> ListReader(DataInputStream dis, ItemReader<T> reader) {
-        try {
-            int size = dis.readInt();
-            List<T> list = new ArrayList<>(size);
-            for (int i = 0; i < size; i++) {
-                list.add(reader.read());
-            }
-            return list;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    private <T> List<T> readList(DataInputStream dis, ItemReader<T> reader) throws IOException {
+        int size = dis.readInt();
+        List<T> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            list.add(reader.read());
         }
+        return list;
     }
 }
