3cac03f93ba08dc9ebb2811c563c3105a2a88eb1
Dzianis Kazakevich <nicklazzdev@gmail.com>
2023-12-05 22:58:33 +0300

    Resume CRUD implementation

--- config/init_db_value.sql
+++ config/init_db_value.sql
@@ -4,3 +4,6 @@ INSERT INTO resume(uuid, full_name) VALUES ('95100986-0ad3-435d-bd07-e1bdd7eddc8
 INSERT INTO contact(type, value, resume_uuid) VALUES ('SKYPE', 'skypeTest', 'ee7bd0ab-542e-43e1-9096-11522b2000ad');
 INSERT INTO contact(type, value, resume_uuid) VALUES ('SKYPE', 'skype_1', 'c5069a8e-68fd-4501-b41b-22e6eb57233f');
 INSERT INTO contact(type, value, resume_uuid) VALUES ('PHONE', '+375291232391', 'c5069a8e-68fd-4501-b41b-22e6eb57233f');
+INSERT INTO contact(type, value, resume_uuid) VALUES ('MAIL', 'av@by.ru', 'ee7bd0ab-542e-43e1-9096-11522b2000ad');
+INSERT INTO contact(type, value, resume_uuid) VALUES ('MAIL', 'skype_1@mail.ru', 'c5069a8e-68fd-4501-b41b-22e6eb57233f');
+INSERT INTO contact(type, value, resume_uuid) VALUES ('MAIL', 'test@yandex.ru', '95100986-0ad3-435d-bd07-e1bdd7eddc8c');
--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Writer;
-import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
 
@@ -25,8 +23,25 @@ public class ResumeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("resumes", storage.getAllSorted());
-        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
+        String uuid = request.getParameter("uuid");
+        String action = request.getParameter("action");
+        if (action == null) {
+            request.setAttribute("resumes", storage.getAllSorted());
+            request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
+            return;
+        }
+        Resume r;
+        switch (action) {
+            case "delete" -> {
+                storage.delete(uuid);
+                response.sendRedirect("resume");
+                return;
+            }
+            case "view", "edit" -> r = storage.get(uuid);
+            default -> throw new IllegalArgumentException("Action" + action + " is illegal");
+        }
+        request.setAttribute("resume", r);
+        request.getRequestDispatcher(("view".equals(action) ? "/WEB-INF/jsp/view.jsp" : "/WEB-INF/jsp/edit.jsp")).forward(request, response);
     }
 
     @Override
